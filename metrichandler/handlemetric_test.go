dcfac857e00a5dd730610d9674a6e7ba056226b4
Igor Aleksandrov <ivaleksandrov@avito.ru>
2023-01-23 20:23:12 +0300

    добавлен тест в качестве примера для godoc, cover -> 57.5%

--- metrichandler/handlemetric_test.go
+++ metrichandler/handlemetric_test.go
@@ -1,14 +1,18 @@
 package metrichandler
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 
 	"github.com/IgorAleksandroff/musthave-devops/internal/metricscollection"
+	"github.com/IgorAleksandroff/musthave-devops/internal/metricscollectionentity"
 	"github.com/go-chi/chi"
 )
 
@@ -55,4 +59,68 @@ func Example() {
 	// 200
 	// 0.1
 	// text/html
+}
+
+func ExampleJSON() {
+	metricsUC, _ := metricscollection.NewMetricsCollection(context.Background(), metricscollection.Config{})
+	h := New(metricsUC, "hashKey")
+
+	r := chi.NewRouter()
+	r.HandleFunc("/update/", h.HandleJSONPost)
+	r.HandleFunc("/value/", h.HandleJSONGet)
+
+	metricSave := metricscollectionentity.Metrics{
+		ID:    "name01",
+		MType: "gauge",
+		Value: func() *float64 { v := 0.1; return &v }(),
+	}
+	metricSave.CalcHash(fmt.Sprintf("%s:gauge:%f", metricSave.ID, *metricSave.Value), "hashKey")
+
+	payloadSave, err := json.Marshal(metricSave)
+	if err != nil {
+		log.Println("payload marshal error")
+
+		return
+	}
+
+	resSave := httptest.NewRecorder()
+	reqSave, _ := http.NewRequest(http.MethodPost, "/update/", bytes.NewReader(payloadSave))
+	reqSave.Header.Set(`Content-Type`, `application/json`)
+	r.ServeHTTP(resSave, reqSave)
+	resultSave := resSave.Result()
+	fmt.Println(resultSave.StatusCode)
+	resultSave.Body.Close()
+
+	metricGet := metricscollectionentity.Metrics{
+		ID:    "name01",
+		MType: "gauge",
+	}
+
+	payloadGet, err := json.Marshal(metricGet)
+	if err != nil {
+		log.Println("payload marshal error")
+
+		return
+	}
+
+	resGet := httptest.NewRecorder()
+	reqGet, err := http.NewRequest(http.MethodPost, "/value/", bytes.NewReader(payloadGet))
+	if err != nil {
+		log.Println("create request error")
+	}
+	reqGet.Header.Set(`Content-Type`, `application/json`)
+	r.ServeHTTP(resGet, reqGet)
+	resultGet := resGet.Result()
+	fmt.Println(resultGet.StatusCode)
+
+	metric := metricscollectionentity.Metrics{}
+	reader := json.NewDecoder(resultGet.Body)
+	defer resultGet.Body.Close()
+	reader.Decode(&metric)
+	fmt.Println(*metric.Value)
+
+	// Output:
+	// 200
+	// 200
+	// 0.1
 }
